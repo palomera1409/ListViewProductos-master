7e53196c24a93743623ad7c5b05cbfe45463b79d
palomera <chore9520@hotmail.com>
2018-04-17 12:39:09 -0500

    practica 009

---
+++ app/src/main/java/com/example/ramon/listviewproductos/AgregarActividad.java
@@ -0,0 +1,53 @@
+package com.example.ramon.listviewproductos;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class AgregarActividad extends AppCompatActivity {
+
+    Spinner spinner;
+    EditText nombreprod;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_actividad);
+        String categoria[]={"Electronica","Electronica","Dulceria","Papeleria","Papeleria","Moda","Perfumeria ","Hogar","Electronicos", "Hogar",
+                "Electrodomesticos", "Electrodomesticos","Electronica"};
+        ArrayAdapter<String> adaptador = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,categoria);
+    spinner =(Spinner)findViewById(R.id.spncategoria);
+    spinner.setAdapter(adaptador);
+    nombreprod = (EditText)findViewById(R.id.edtxtnombre);
+
+    }
+
+
+
+
+    public void agregar (View v)
+    {
+
+        Intent  i  = new Intent();
+i.putExtra("nombre",nombreprod.getText().toString());
+i.putExtra("categoria",spinner.getSelectedItem().toString());
+setResult(RESULT_OK,i);
+
+        Toast mensaje = Toast.makeText(getApplicationContext(),"Se agregaron correctamente "+nombreprod.getText().toString().toLowerCase(),Toast.LENGTH_LONG);
+        mensaje.setGravity(Gravity.CENTER,32,32);
+        mensaje.setDuration(Toast.LENGTH_LONG);
+        mensaje.show();
+finish();
+
+
+    }
+
+
+
+}
--- app/src/main/java/com/example/ramon/listviewproductos/MainActivity.java
+++ app/src/main/java/com/example/ramon/listviewproductos/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ramon.listviewproductos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,12 +9,16 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private TextView txt;
     private ListView lista;
     
     //Datos
-    private String productos[] = {
+    /*private String productos[] = {
             "Computadora", "Mouse", "Dulces", "Hojas",
             "Lapices", "Lentes","Reloj", "Cuchara", "Celular", "Mesa", "Refrigerador",
             "Horno", "Audifonos"
@@ -21,7 +26,10 @@ public class MainActivity extends AppCompatActivity {
     private String categoria[] = {
             "Electronica","Electronica","Dulceria","Papeleria","Papeleria","Moda","Perfumeria ","Hogar",
             "Electronicos", "Hogar", "Electrodomesticos", "Electrodomesticos","Electronica"
-    };
+    };*/
+
+    private List<String> lproductos = new ArrayList<>();
+    private  List<String> lcategorias = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,15 +38,46 @@ public class MainActivity extends AppCompatActivity {
         txt = (TextView) findViewById(R.id.textView);
         lista = (ListView) findViewById(R.id.lista);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,productos);
+
+    }
+
+
+    public void llamaactividad (View x)
+    {
+        Intent i= new Intent(this, AgregarActividad.class);
+        startActivityForResult(i,777);
+
+
+    }
+
+
+    public  void  actualizarTabla()
+    {
+      String productos[] = new String[lproductos.size()];
+      lproductos.toArray(productos);
+
+              ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,productos);
         lista.setAdapter(adapter);
 
         lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long
                     id) {
-                txt.setText("Categoria elegida -" + categoria[position]);
+                txt.setText("Categoria elegida -" + lcategorias.get(position));
             }
         });
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+   lproductos.add(data.getStringExtra("nombre"));
+   lcategorias.add(data.getStringExtra("categoria"));
+
+    actualizarTabla();
+
     }
 }
